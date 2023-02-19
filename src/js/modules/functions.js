519abd7a604ee71bccc1c804c9a79b3585e5a620
Samar <cyparis.samura@yandex.ru>
2023-02-19 13:05:53 +0300

    Added soft-corp repository

---
+++ src/js/modules/functions.js
@@ -0,0 +1,85 @@
+export function isWebp() {
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	testWebP(function (support) {
+		if (support == true) {
+			document.querySelector('body').classList.add('webp');
+		} else {
+			document.querySelector('body').classList.add('no-webp');
+		}
+	});
+}
+
+
+// Burger menu
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+	});
+}
+
+
+// Scrolling when clicked
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0){
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+	
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		// Go to the desired section after clicking on the link
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight + 1;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+			// Adds an active link after click
+			menuLinks.forEach(removeLink => {
+				removeLink.classList.remove('_active');
+				if (Array.from(menuLinks).find(findLink => findLink.isEqualNode(menuLink))) {
+					menuLink.classList.add('_active');
+				}
+			});
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
+
+
+// Select
+const selectSingle = document.querySelector('.select');
+const selectSingle_title = selectSingle.querySelector('.select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.select__label');
+// Toggle select menu
+selectSingle_title.addEventListener('click', () => {
+	if ('active' === selectSingle.getAttribute('data-state')) {
+		selectSingle.setAttribute('data-state', '');
+	} else {
+		selectSingle.setAttribute('data-state', 'active');
+	}
+});
+// Close select content when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+	selectSingle_labels[i].addEventListener('click', (evt) => {
+		selectSingle_title.textContent = evt.target.textContent;
+		selectSingle.setAttribute('data-state', '');
+	});
+}
